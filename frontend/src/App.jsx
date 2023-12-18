4d48be5fb16b4a16b53fc00114b460a18175ab21
Dav.O <davolufuwa@gmail.com>
2023-12-18 13:12:23 +0100

    added tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Button } from  '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
       <div>Yeppie</div>
-      
+      <Button className='bg-red-400'>Okay Nau</Button>
     </>
   )
 }
